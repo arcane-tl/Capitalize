cf7d338e93ef57e2b92c0782ab3ae05d9f746318
Tomi Lindroos <tomi@digileap.fi>
2025-03-24 12:02:47 +0200

    activated environmental files for the app

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBOE4c3ZeP0L2UhHQrKkEG9W9X7Kfx-704",
-  authDomain: "capitalize-39317.firebaseapp.com",
-  projectId: "capitalize-39317",
-  storageBucket: "capitalize-39317.appspot.com",
-  messagingSenderId: "23029306910",
-  appId: "1:23029306910:web:d94aa707fa359e02bf000f",
-  measurementId: "G-78VY7QJX6M",
-  databaseURL: "https://capitalize-39317-default-rtdb.europe-west1.firebasedatabase.app/"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 };
